7800f3dc58d497d20f927b6c95fdbdd38a49d2a4
Lucas Alves <lucasalvesbs6@gmail.com>
2022-05-30 10:10:58 -0300

    feat(heroku): add heroku configurations

--- ormconfig.js
+++ ormconfig.js
@@ -9,8 +9,15 @@ module.exports = {
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
-  entities: ['src/app/**/*.entity.ts'],
-  migrations: ['src/config/typeorm/migrations/*.ts'],
+  ssl: process.env.NODE_ENV === 'production' ? true : false,
+  extra:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: { rejectUnauthorized: false },
+        }
+      : null,
+  entities: [process.env.ENTITIES],
+  migrations: [process.env.MIGRATIONS],
   cli: {
     entitiesDir: 'src/app/**',
     migrationsDir: 'src/config/typeorm/migrations',
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { UsersModule } from './app/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot(/* {
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
@@ -17,7 +17,7 @@ import { UsersModule } from './app/users/users.module';
       database: process.env.DB_NAME,
       synchronize: false,
       logging: false,
-    }),
+    } */),
     UsersModule,
   ],
   controllers: [AppController],
