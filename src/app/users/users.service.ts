eceefeb36941c6bb35c17384155fda67033bb6a6
Lucas Alves <lucasalvesbs6@gmail.com>
2022-06-06 12:05:32 -0300

    feat(app): add cpf property in GET method

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -16,7 +16,7 @@ export class UsersService {
 
   async findAllUsers() {
     return await createQueryBuilder(UsersEntity, 'users')
-      .select(['users.id', 'users.fullName'])
+      .select(['users.id', 'users.fullName', 'users.cpf'])
       .getMany();
   }
 
