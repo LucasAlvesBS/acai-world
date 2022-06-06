25841dec76a631f530a1a74abaf5f8a002dff5c4
Lucas Alves <lucasalvesbs6@gmail.com>
2022-06-06 09:41:26 -0300

    feat(security): add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 3000;
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  const port = process.env.PORT || 3000;
+  const run = new Logger('NestApplication');
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(port, () => run.log(`API is running on port ${port}`));
 }
 bootstrap();
