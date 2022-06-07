c7a827f29bcd6a3c71a5769f911461d57b4d48e4
Lucas Alves <lucasalvesbs6@gmail.com>
2022-06-07 10:12:52 -0300

    style(helpers): add cpf_valid message

--- src/helpers/message.helper.ts
+++ src/helpers/message.helper.ts
@@ -1,6 +1,7 @@
 export const MessageHelper = {
   PASSWORD_VALID:
     'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
+  CPF_VALID: 'O cpf deve conter 11 números',
   PASSWORD_OR_EMAIL_INVALID: 'Usuário ou senha inválidos',
   UNIDENTIFIED_USER: 'Usuário não identificado',
   NOT_FOUND: 'Página não encontrada',
